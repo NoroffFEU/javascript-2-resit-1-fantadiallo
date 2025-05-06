c32d2b63d5bb8f50025e383484052f689e567116
fantadiallo <fatmbo0003@outlook.com>
2025-05-06 21:16:58 +0200

    feat: render game detail page based on ID param

---
+++ src/js/api/router/views/game.js
@@ -0,0 +1,5 @@
+import renderGameDetails from "../../../ui/render/GameDetails";
+
+renderGameDetails
+const gameId = new URLSearchParams(window.location.search).get("id");
+renderGameDetails(gameId);
